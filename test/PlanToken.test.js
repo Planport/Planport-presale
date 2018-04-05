617c961db8572cbaa3ceccdd4aea7bc2a7d8b501
leonardogedler <leogedler@hotmail.com>
2018-04-04 22:08:16 -0400

    Added more tests

--- test/PlanToken.test.js
+++ test/PlanToken.test.js
@@ -25,5 +25,42 @@ describe('PlanToken', () => {
     it('deploys the token', () =>{
         assert.ok(planToken.options.address);
     });
+
+    it('has 500.000.000 millions token supply', async () =>{
+        const totalSupply = await planToken.methods.totalSupply().call();
+        assert.equal('500000000', web3.utils.fromWei(totalSupply, 'ether'));
+    });
+
+    it('gives owner total supply', async()=>{
+        const balance = await planToken.methods.balanceOf(accounts[0]).call();
+        assert.equal('500000000', web3.utils.fromWei(balance, 'ether'));
+    });
+
+    it('transfer token from owner account to other account', async()=>{
+        const transfer = await planToken.methods.transfer(accounts[1], web3.utils.toWei('1000', 'ether')).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        const balance = await planToken.methods.balanceOf(accounts[1]).call();
+        assert.equal('1000', web3.utils.fromWei(balance, 'ether'));
+    });
+
+    it('can tranfer ownership', async()=>{
+
+        const transferOwnership = await planToken.methods.transferOwnership(accounts[1]).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        const acceptOwnership = await planToken.methods.acceptOwnership().send({
+            from: accounts[1],
+            gas: '1000000'
+        });
+
+        const newOwner = await planToken.methods.owner().call();
+        assert.equal(accounts[1], newOwner);
+
+    })
     
 })
