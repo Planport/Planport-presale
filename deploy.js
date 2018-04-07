64c30db40df2a6969909a0ab0ac3260fd5887b5e
leonardogedler <leogedler@hotmail.com>
2018-04-07 09:03:14 -0400

    Added two more tests

--- deploy.js
+++ deploy.js
@@ -11,11 +11,13 @@ const web3 = new Web3(provider);
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Deployment Account', accounts[0]);
+    // Account to deploy
+    const account = accounts[0];
+    console.log('Deployment Account', account);
 
     const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
-        .send({ gas: '1000000', from: accounts[0]});
+        .send({ gas: '1000000', from: account});
 
     console.log('***TOKEN ADDRESS*** : ', result.options.address); 
     
--- test/PlanToken.test.js
+++ test/PlanToken.test.js
@@ -36,7 +36,7 @@ describe('PlanToken', () => {
         assert.equal('500000000', web3.utils.fromWei(balance, 'ether'));
     });
 
-    it('transfer token from owner account to other account', async()=>{
+    it('transfer token from owner account to another account', async()=>{
         const transfer = await planToken.methods.transfer(accounts[1], web3.utils.toWei('1000', 'ether')).send({
             from: accounts[0],
             gas: '1000000'
@@ -46,7 +46,34 @@ describe('PlanToken', () => {
         assert.equal('1000', web3.utils.fromWei(balance, 'ether'));
     });
 
-    it('can tranfer ownership', async()=>{
+    it('transfer token from one account to another account', async()=>{
+        const transfer = await planToken.methods.transfer(accounts[1], web3.utils.toWei('1000', 'ether')).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        const transfer2 = await planToken.methods.transfer(accounts[2], web3.utils.toWei('100', 'ether')).send({
+            from: accounts[1],
+            gas: '1000000'
+        });
+
+        const balance = await planToken.methods.balanceOf(accounts[2]).call();
+        assert.equal('100', web3.utils.fromWei(balance, 'ether'));
+    });
+
+    it('cant transfer tokens if an account doesnt have tokens', async()=>{
+        try {
+            const transfer = await planToken.methods.transfer(accounts[2], web3.utils.toWei('100', 'ether')).send({
+                from: accounts[3],
+                gas: '1000000'
+            });
+            assert(false);
+        } catch (error) {
+            assert.ok(error);
+        }
+    });
+
+    it('can transfer ownership', async()=>{
 
         const transferOwnership = await planToken.methods.transferOwnership(accounts[1]).send({
             from: accounts[0],
