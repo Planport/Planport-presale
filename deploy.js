67acf61d39131c31d5cedc87e9d83d4e11c2bdd3
leonardogedler <leogedler@hotmail.com>
2018-04-10 17:08:57 -0400

    Added ABI logging

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 const compiledFactory = require('./build/PlanToken.json');
 
 const provider = new HDWalletProvider(
-    'Mnemonic .... ... ... ... ... ...',
+    'Mnemonic ... ... ... ... ...',
     'https://rinkeby.infura.io/qCPxJQV2ZokAOV6SzbMG'
 );
 const web3 = new Web3(provider);
@@ -13,12 +13,13 @@ const deploy = async () => {
 
     // Account to deploy
     const account = accounts[0];
-    console.log('Deployment Account', account);
-
+    
     const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-        .deploy({ data: compiledFactory.bytecode })
-        .send({ gas: '1000000', from: account});
-
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: account});
+    
+    console.log('TOKEN ABI', compiledFactory.interface);
+    console.log('Deployment Account', account);
     console.log('***TOKEN ADDRESS*** : ', result.options.address); 
     
 };
